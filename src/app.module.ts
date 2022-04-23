90d9700cf6943b82094801d1d85df2f2b8710abf
Kevin Totesaut <kevin.totesaut@geopagos.com>
2022-04-23 17:47:12 -0300

    feature/prod-migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,6 +15,7 @@ import config from './config';
     TypeOrmModule.forRootAsync({
       useFactory: async () =>
         Object.assign(await getConnectionOptions(), {
+          ssl: false,
           entities: ['dist/**/*.entity.js'],
           migrations: ['dist/migrations/**/*.js'],
         }),
