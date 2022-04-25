21fa46e146a9e06ac7e26ebb57f98555ea30b037
Kevin Totesaut <kevin.totesaut@geopagos.com>
2022-04-25 17:13:28 -0300

    invoices.controller

--- src/invoices/invoinces.dto.ts
+++ src/invoices/invoinces.dto.ts
@@ -1,5 +1,62 @@
-import { IsOptional, IsPositive, Min } from 'class-validator';
+import {
+  IsString,
+  IsNotEmpty,
+  IsNumber,
+  IsDateString,
+  IsOptional,
+  IsPositive,
+  Min,
+} from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateInvoinceDto {
+  @IsNumber()
+  @IsNotEmpty()
+  @ApiProperty()
+  readonly originId: number;
+
+  @IsNumber()
+  @ApiProperty()
+  @IsNotEmpty()
+  readonly vendorId: number;
+
+  @IsString()
+  @ApiProperty()
+  @IsNotEmpty()
+  readonly invoiceNumber: string;
+
+  @IsDateString()
+  @ApiProperty()
+  readonly invoiceDate: string;
+
+  @IsNumber()
+  @ApiProperty()
+  readonly invoiceTotal: number;
+
+  @IsNumber()
+  @ApiProperty()
+  readonly paymentTotal: number;
+
+  @IsNumber()
+  @ApiProperty()
+  readonly creditTotal: number;
+
+  @IsNumber()
+  @ApiProperty()
+  readonly bankId: number;
+
+  @IsDateString()
+  @ApiProperty()
+  readonly invoiceDueDate: string;
+
+  @IsDateString()
+  @ApiProperty()
+  readonly paymentDate: string;
+
+  @IsString()
+  @ApiProperty()
+  readonly currency: string;
+}
 
 export class FilterInvoicesDto {
   @IsOptional()
